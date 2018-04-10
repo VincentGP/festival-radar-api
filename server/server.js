b8d75d7f6aa000ab281561962502d07d3210ffe0
Vincent Gercke Pedersen <vgercke@gmail.com>
2018-04-10 14:25:43 +0200

    Første commit 😇

---
+++ server/server.js
@@ -0,0 +1,23 @@
+const port = process.env.PORT || 7777;
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+// Sæt Expess i gang
+const app = express();
+
+app.use(bodyParser.json());
+
+// Hent alle festivaler
+app.get('/festivals', (req, res) => {
+  res.status(200).send('Her er en masse festivaler');
+});
+
+app.post('/festivals', (req, res) => {
+  res.status(200).send(req.body.navn);
+});
+
+// Start server
+app.listen(port, () => {
+  console.log(`Startede server 😛`);
+});
